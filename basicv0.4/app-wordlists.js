4079d0c589651398fc20bbc6f4badeaec0e11170
funderburkjim <funderburkjim@gmail.com>
2018-09-10 18:11:13 -0400

    change relative path from ../wordlists to wordlists

--- basicv0.4/app-wordlists.js
+++ basicv0.4/app-wordlists.js
@@ -1,10 +1,10 @@
 var app_wordlists = [
  {name:'Hitopadesha: invocation', input:'slp1', comment:'0044-0064',
-   url:'../wordlists/hitopadesha01.txt',
+   url:'wordlists/hitopadesha01.txt',
  },
  {name:'Hitopadesha: introduction-a',input:'slp1',
   comment:'0065 - 0099',
-  url:'../wordlists/hitopadesha02.txt',
+  url:'wordlists/hitopadesha02.txt',
 },
- {name:'Testiast',input:'iast',url:'../wordlists/testiast.txt'}
+ {name:'Testiast',input:'iast',url:'wordlists/testiast.txt'}
 ]

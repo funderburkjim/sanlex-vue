b28ebe5ae6e584288ffac101e6d28aa8a55dabb4
funderburkjim <funderburkjim@users.noreply.github.com>
2018-09-11 12:42:25 -0400

    Update app-wordlists.js

--- basicv0.4/app-wordlists.js
+++ basicv0.4/app-wordlists.js
@@ -1,10 +1,10 @@
 var app_wordlists = [
- {name:'Hitopadesha: invocation', input:'slp1', comment:'0044-0064',
-   url:'wordlists/hitopadesha01.txt',
- },
- {name:'Hitopadesha: introduction-a',input:'slp1',
-  comment:'0065 - 0099',
-  url:'wordlists/hitopadesha02.txt',
-},
- {name:'Testiast',input:'iast',url:'wordlists/testiast.txt'}
+ {name:'Hitopadesh01', input:'slp1', comment:'0044-0064', url:'wordlists/hitopadesha01.txt', },
+ {name:'Hitopadesha02',input:'slp1', comment:'0065 - 0099', url:'wordlists/hitopadesha02.txt',},
+ {name:'Hitopadesha03',input:'slp1', comment:'0065 - 0099', url:'wordlists/hitopadesha03.txt',},
+ {name:'Hitopadesha04',input:'slp1', comment:'0065 - 0099', url:'wordlists/hitopadesha04.txt',},
+ {name:'Hitopadesha05',input:'slp1', comment:'0065 - 0099', url:'wordlists/hitopadesha05.txt',},
+ {name:'Hitopadesha06',input:'slp1', comment:'0065 - 0099', url:'wordlists/hitopadesha06.txt',},
+ {name:'Hitopadesha07',input:'slp1', comment:'0065 - 0099', url:'wordlists/hitopadesha07.txt',},
+ //{name:'Testiast',input:'iast',url:'wordlists/testiast.txt'}
 ]
